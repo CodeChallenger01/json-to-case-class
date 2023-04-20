1b79736461a4332e2954717959faef93200718a3
Manish Mishra <manish.mishra@knoldus.com>
2023-04-20 15:02:11 +0530

    Added case class to json automatically

--- src/main/scala/CaseClassToJsonAutomatically.scala
+++ src/main/scala/CaseClassToJsonAutomatically.scala
@@ -1,7 +1,13 @@
-import play.api.libs.json.Json
+
+import play.api.libs.json.{Json, OWrites}
+
+case class Person(name: String, id: Int, address: String)
+
+object Person {
+  implicit val residentWrites: OWrites[Person] = Json.writes[Person]
+}
 
 object Main extends App {
-  implicit val residentWrites = Json.writes[Person]
   private val person = Person(name = "Manish Mishra", id = 21, address = "Noida")
   private val personJson = Json.toJson(person)
   println(personJson)
