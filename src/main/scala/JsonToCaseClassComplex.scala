a86011453a7e9ebfa5a6fe060c0f81465f25e2f5
Manish Mishra <manish.mishra@knoldus.com>
2023-04-24 18:14:51 +0530

    Added json to case clas

--- src/main/scala/JsonToCaseClassComplex.scala
+++ src/main/scala/JsonToCaseClassComplex.scala
@@ -1,6 +1,11 @@
+import com.knoldus.{CEO, Role}
 import play.api.libs.json.{JsError, JsResult, JsSuccess, JsValue, Json, Reads}
-case class Employee(name: String, middleName: Option[String], id: Int, phoneNumber: List[Long], address: String, achievements: Awards)
+
+case class Employee(name: String, middleName: Option[String], id: Int, phoneNumber: List[Long], address: String, achievements: Awards, role: Role)
+
 object JsonToCaseClassComplex extends App {
+  implicit val ceo: Reads[CEO] = Json.reads[CEO]
+  implicit val role: Reads[Role] = Json.reads[Role]
   implicit val award: Reads[Awards] = Json.reads[Awards]
   implicit val employee: Reads[Employee] = Json.reads[Employee]
   private val employeeDetail: JsValue = Json.parse {
@@ -9,12 +14,20 @@ object JsonToCaseClassComplex extends App {
       |"id" : 1849,
       |"phoneNumber" : [9897878987,8767676567],
       |"address" : "Noida",
-      |"achievements" :{"awardOne":" Give Little Unexpected Extra","awardTwo":" Go 1%"}
+      |"achievements" :{"awardOne":" Give Little Unexpected Extra","awardTwo":" Go 1%"},
+      |"role":{"_type":"com.knoldus.CEO","role":"Head"}
       |}""".stripMargin
   }
   private val jsonToCaseClass: JsResult[Employee] = Json.fromJson[Employee](employeeDetail)
   val result = jsonToCaseClass match {
-    case JsSuccess(value, path) => s"Name: ${value.name}\nMiddleName :${value.middleName}\nId :${value.id}\nPhone Number :${value.phoneNumber}\nAddress :${value.address}\nAchievements\t1 :${value.achievements.awardOne}\t2: ${value.achievements.awardTwo}"
+    case JsSuccess(value, path) =>
+      s"""Name: ${value.name}
+         |MiddleName :${value.middleName}
+         |Id :${value.id}\nPhone Number :${value.phoneNumber}
+         |Address :${value.address}
+         |Achievements\t1 :${value.achievements.awardOne}\t2: ${value.achievements.awardTwo}
+         |Role : ${value.role.toString}
+         |""".stripMargin
     case JsError(errors) => s"Error : ${JsError.toJson(errors).toString}"
   }
   println(result)
