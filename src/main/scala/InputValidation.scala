2f97544e50c3ab2cd6a1e00cd5ac55f6d6d1e4d0
Manish Mishra <manish.mishra@knoldus.com>
2023-04-24 12:06:13 +0530

    Remove implicitly and added meaningful error message

--- src/main/scala/InputValidation.scala
+++ src/main/scala/InputValidation.scala
@@ -1,21 +1,21 @@
-import com.knoldus.{Check, NumberTooLong, StringContainSpecialCharacter, StringTooLong}
+import com.knoldus.{Error, NumberTooLong, StringContainNumber, StringContainSpecialCharacter, StringTooLong}
 
 import scala.io.StdIn.readLine
 
 trait Validator[A] {
-  def validate(input: A): Either[String, String]
+  def validate(input: A): Either[Error, String]
 }
 
 case class StringValidation(userInput: String, maxLength: Int)
 
 object StringValidation {
   implicit val string = new Validator[StringValidation] {
-    override def validate(input: StringValidation): Either[String, String] = {
+    override def validate(input: StringValidation): Either[Error, String] = {
       val content = input.userInput
       content match {
-        case value if value.isEmpty || content.length > input.maxLength => Left(Check.check(StringTooLong("String is Too Long")))
-        case value if value.contains("@") || value.contains("$") || value.contains("#") => Left(Check.check(StringContainSpecialCharacter("String Containing Special Character")))
-        case value => Right(s"Valid String : $value")
+        case value if value.isEmpty || content.length > input.maxLength => Left(StringTooLong("String is Too Long"))
+        case value if value.contains("@") || value.contains("$") || value.contains("#") => Left(StringContainSpecialCharacter("String Containing Special Character"))
+        case value => Right(s"Validated String : $value")
       }
     }
   }
@@ -25,34 +25,46 @@ case class NumberValidation(userInput: Int, minRange: Int, maxRange: Int)
 
 object NumberValidation {
   implicit val number = new Validator[NumberValidation] {
-    override def validate(input: NumberValidation): Either[String, String] = {
+    override def validate(input: NumberValidation): Either[Error, String] = {
       val content = input.userInput
       content match {
-        case value if value.isNaN || content < input.minRange || content > input.maxRange => Left(Check.check(NumberTooLong(" Number is out of Range ")))
+        case value if value.isNaN || content < input.minRange || content > input.maxRange => Left(NumberTooLong(" Number is out of Range "))
         case value => Right(s"Valid Number: $value")
       }
     }
   }
 }
 
+object InputValidation {
+  def inputValidate[T](input: T)(implicit S: Validator[T]): Either[Error, String] = {
+    S.validate(input)
+  }
+}
+
 object MainInput extends App {
   /* Checking For String Input */
   private val inputOne = readLine("Enter the String :")
-  private val string: StringValidation = StringValidation(inputOne, 10)
-  private val isStringValidated = implicitly[Validator[StringValidation]].validate(string)
+  private val string: StringValidation = StringValidation(inputOne, 20)
+  private val isStringValidated = InputValidation.inputValidate(string)
   private val resultOfString = isStringValidated match {
-    case Left(value) => s"$value"
     case Right(value) => s"$value"
+    case Left(value) => value match {
+      case StringContainSpecialCharacter(message) => s"$message"
+      case StringTooLong(message) => s"$message"
+      case StringContainNumber(message) => s"$message"
+    }
   }
   println(resultOfString)
 
   /* Checking For Numeric Input */
   private val inputTwo = readLine("\nEnter the Number :").toInt
   private val number: NumberValidation = NumberValidation(inputTwo, 10, 1000)
-  private val isNumberValidated = implicitly[Validator[NumberValidation]].validate(number)
+  private val isNumberValidated = InputValidation.inputValidate(number)
   private val resultOfNumber = isNumberValidated match {
-    case Left(value) => s"$value"
     case Right(value) => s"$value"
+    case Left(value) => value match {
+      case NumberTooLong(message) => s"$message"
+    }
   }
   println(resultOfNumber)
 
