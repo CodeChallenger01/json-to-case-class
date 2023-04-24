f85cf9e48b5329236411da2c6424cd93624857c5
Manish Mishra <manish.mishra@knoldus.com>
2023-04-24 18:14:28 +0530

    Added case lass to json complex

--- src/main/scala/CaseClassToJsonComplex.scala
+++ src/main/scala/CaseClassToJsonComplex.scala
@@ -1,9 +1,11 @@
+import com.knoldus.{CEO, Role}
 import play.api.libs.json.{Json, OWrites}
 
-case class Employee(name: String, middleName: Option[String], id: Int, phoneNumber: List[Long], address: String, achievements: Awards)
+case class People(name: String, middleName: Option[String], id: Int, phoneNumber: List[Long], address: String, achievements: Awards, role: Role)
 
-object Employee {
-  implicit val write: OWrites[Employee] = Json.writes[Employee]
+object People {
+  implicit val role: OWrites[Role] = Json.writes[Role]
+  implicit val write: OWrites[People] = Json.writes[People]
 }
 
 case class Awards(awardOne: String, awardTwo: String)
@@ -13,8 +15,10 @@ object Awards {
 }
 
 object CaseClassToJsonComplex extends App {
+
+  implicit val ceo: OWrites[CEO] = Json.writes[CEO]
   private val awards = Awards("Give Little Unexpected Extra", "Go 1%")
-  private val employee = Employee("Manish Mishra",None, 1849, List(9897878987L, 8767676567L), "Noida", awards)
+  private val employee = People("Manish Mishra", None, 1849, List(9897878987L, 8767676567L), "Noida", awards, CEO("Head"))
   private val parseToJson = Json.toJson(employee)
   println(parseToJson)
 }
